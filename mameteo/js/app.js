07e52a47d6120f5f19ab9196f621fcd56f5f3744
Pierre <pierre.linz@orange.fr>
2022-02-01 12:20:27 +0100

    upd

--- mameteo/js/app.js
+++ mameteo/js/app.js
@@ -1,18 +1,17 @@
 //Mécanisme de routage :
-angular.module("app", ["ngRoute"]);
-var app = angular.module("myApp", ["ngRoute]"])
+var app = angular.module("myApp", ["ngRoute"]);
 app.config(function($routeProvider) {
-  $routeProvider
-  .when("/", {
-    templateUrl : "../partials/meteovilles.html" //default ?
-  })
-  .when("/meteovilles", {
-    templateUrl : "../partials/meteovilles.html"
-  })
-  .when("/previsions", {
-    templateUrl : "../partials/previsions.html"
-  })
-  .when("/villes", {
-    templateUrl : "../partials/villes.html"
-  });
+    $routeProvider
+        .when("/", {
+            templateUrl : "/../partials/meteovilles.html"
+        })
+        .when("/previsions", {
+            templateUrl : "/../partials/previsions.html"
+        })
+        .when("/villes", {
+            templateUrl : "/../partials/villes.html"
+        })
+        .when("/meteovilles", {
+            templateUrl : "/../partials/meteovilles.html"
+        });
 });
