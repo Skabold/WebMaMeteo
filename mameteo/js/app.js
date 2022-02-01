5000b1c701e453b4377b40010a71c3c19f1f0985
Pierre <pierre.linz@orange.fr>
2022-02-01 11:37:04 +0100

    Initial commit

---
+++ mameteo/js/app.js
@@ -0,0 +1,18 @@
+
+//Mécanisme de routage :
+var app = angular.module("myApp", ["ngRoute"]);
+app.config(function($routeProvider) {
+  $routeProvider
+  .when("/", {
+    templateUrl : "/partials/meteovilles.html" //default ?
+  })
+  .when("/meteovilles", {
+    templateUrl : "/partials/meteovilles.html"
+  })
+  .when("/previsions", {
+    templateUrl : "/partials/previsions.html"
+  })
+  .when("/villes", {
+    templateUrl : "/partials/villes.html"
+  });
+});
